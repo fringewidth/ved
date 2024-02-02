b6ecd6139af68d5afe8d862560ff890f78b93cca
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-02-02 18:39:01 +0530

    Implement scrolling animation

--- src/Components/HeroContent.jsx
+++ src/Components/HeroContent.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import VideoScroll from "./VideoScroll";
 
 export default function HeroContent() {
+  const containerRef = useRef(null);
   return (
-    <div className="herocontent">
-      <VideoScroll src="src/assets/video/vedas-opening.mp4" />
+    <div className="herocontent" ref={containerRef}>
+      <VideoScroll
+        src="src/assets/video/vedas-opening.mp4"
+        parentRef={containerRef}
+        triggerHeightPercentage="80"
+      />
       <div className="herotext">
         <p>
           Introducing <span className="specialtext">Ved.</span>
--- src/Components/VideoScroll.jsx
+++ src/Components/VideoScroll.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 export default function VideoScroll(props) {
+  const videoRef = useRef(null);
+  const [duration, setDuration] = useState();
+
+  const handleScroll = () => {
+    if (props.parentRef.current && videoRef.current) {
+      const parentOffsetTop = props.parentRef.current.offsetTop;
+      const parentHeight = props.parentRef.current.clientHeight;
+      const triggerFull =
+        (parentHeight * props.triggerHeightPercentage) / 100 -
+        window.innerHeight / 2;
+      const scrolledHeight = window.scrollY;
+      const timeToSeek = (duration * scrolledHeight) / triggerFull;
+      console.log(timeToSeek);
+      if (videoRef.current.currentTime != triggerFull) {
+        videoRef.current.currentTime = timeToSeek;
+        videoRef.current.pause();
+      }
+    }
+  };
+
+  useEffect(() => {
+    const handleMetadata = () => {
+      if (videoRef.current) {
+        setDuration(videoRef.current.duration);
+      }
+    };
+
+    videoRef.current.addEventListener("loadedmetadata", handleMetadata);
+    window.addEventListener("scroll", handleScroll);
+  }, [duration]);
+
   return (
-    <video src={props.src} autoplay>
+    <video ref={videoRef}>
+      <source src={props.src} type="video/mp4" />
       <p>Inspired by the tradition of the Vedas,</p>
     </video>
   );
