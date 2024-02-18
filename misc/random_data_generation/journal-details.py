5eb1febb019616f71d86a1e9c6109ad63d1c62cd
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-02-18 18:41:41 +0530

    Save in xlsx format

--- misc/random_data_generation/journal-details.py
+++ misc/random_data_generation/journal-details.py
@@ -1,7 +1,7 @@
-import random
+import random, openpyxl
 
 def generate_details(field):
-    name = random.choice([f"International Journal of {field}", f"Advances in {field}", f"Journal of {field} Quaterly"])
+    name = random.choice([f"International Journal of {field}", f"Advances in {field}", f"Journal of {field} Quarterly"])
     issn = "".join(random.choice("abcdefghijklmnopqrstuvwxyz1234567890") for _ in range(10))
     publisher = random.choice(["Springer", "Elsevier", "IEEE", "Wiley", "Taylor & Francis"])
     impact_factor = round(random.uniform(0.5, 5), 2)
@@ -11,8 +11,17 @@ def generate_details(field):
     contact_email = random.choice(["editor", "info", "contact"]) + f"@{website}"
     review_process = random.choice(["Single-blind", "Double-blind", "Open"])
 
-    return [name, issn, publisher, impact_factor, website, country, language, contact_email, review_process]
+    return [name, issn, publisher, impact_factor, website, country, language, contact_email, review_process, field]
 
-for i in generate_details("Computer Science"):
-    print(i)
+src_wb = openpyxl.load_workbook("D:\journal_fields.xlsx")
+src_ws = src_wb.active
+dest_wb = openpyxl.Workbook()
+dest_ws = dest_wb.active
+
+for i in range(1, src_ws.max_row+1):
+    field = src_ws.cell(row=i, column=1).value
+    details = generate_details(field)
+    dest_ws.append(details)
+
+dest_wb.save("D:\journals.xlsx")
 
