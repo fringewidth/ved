fdf4a0ad6417a9d7e931f4592bbeae92400d2d6d
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-02-20 11:31:32 +0530

    Add authenticate page

--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 import NavBar from "./NavBar";
 export default function Authenticate(props) {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <div class="authpage">
+        <video
+          src="src/assets/video/auth-page-animation.mp4"
+          muted
+          autoPlay
+          loop
+        >
+          <source src={props.src} type="video/mp4"></source>
+          <p>Your browser does not support video</p>
+        </video>
+        <div>
+          <form action="" class="form">
+            <p class="specialtext">Welcome back!</p>
+            <input type="text" placeholder="username" class="input"></input>
+            <input type="password" placeholder="password" class="input"></input>
+            <input type="submit" value="Login" class="submit button"></input>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
