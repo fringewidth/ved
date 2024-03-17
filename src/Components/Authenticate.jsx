b672bf71114555f228bf1cf081058d0ccd0d1822
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-03-17 08:05:14 +0530

    Display auth errors

--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import { getSupabase } from "../utils/supabaseClient";
 import NavBar from "./NavBar";
 import { useParams } from "react-router-dom";
 import authPageAnimation from "../assets/video/auth-page-animation.mp4";
-import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-const supabaseUrl = "https://txouxmylhwoxcyciynby.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = getSupabase();
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const initialInputs = { email: "", password: "" };
 
 export default function Authenticate(props) {
   const { authtype } = useParams();
-  const [inputs, setInputs] = useState({ email: "", password: "" });
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  useEffect(() => {
+    setInputs(initialInputs);
+    setErrorMsg("");
+    emailRef.current.style.border = "none";
+    passwordRef.current.style.border = "none";
+  }, [authtype]);
+  const [inputs, setInputs] = useState(initialInputs);
+  const [errorMsg, setErrorMsg] = useState("");
   const navigate = useNavigate();
 
-  // const navbarButtons = {
-  //   signup: authtype === "login",
-  //   login: authtype === "signup",
-  // };
+  const handleError = (error) => {
+    setErrorMsg(error.message);
+  };
 
   const handleInputChange = (e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
@@ -27,33 +34,45 @@ export default function Authenticate(props) {
 
   const login = async (e) => {
     e.preventDefault();
+
+    // sign in user
     const { data, error } = await supabase.auth.signInWithPassword({
       email: inputs.email,
       password: inputs.password,
     });
+    if (error) {
+      handleError(error);
+    }
     if (data.user === null) {
+      // user does not exist
       e.target.email.style.border = "1px solid red";
       e.target.password.style.border = "1px solid red";
     } else {
       e.target.email.style.border = "none";
+
+      // fetch username and navigate to user page
       const { data, error } = await supabase
         .from("researchers")
         .select("username")
         .eq("email", inputs.email);
 
-      navigate(`/user/:${data[0].username}`);
+      if (error) {
+        handleError(error);
+      } else navigate(`/user/:${data[0].username}`);
     }
   };
+
   const signup = async (e) => {
     e.preventDefault();
+    // sign up user
     const { data, error } = await supabase.auth.signUp({
       email: inputs.email,
       password: inputs.password,
     });
     if (error) {
-      alert(error.message);
+      handleError(error);
     }
-    console.log(data, error);
+
     if (data.user === null) {
       e.target.email.style.border = "1px solid red";
       e.target.password.style.border = "1px solid red";
@@ -87,30 +106,16 @@ export default function Authenticate(props) {
         <div>
           <form action="" className="form" onSubmit={authValues.handleSubmit}>
             <p className="specialtext">{authValues.greeting}</p>
-            {/* {authtype === ":signup" ? (
-              <>
-                <input
-                  type="text"
-                  placeholder="Full Name"
-                  name="fullname"
-                  className="input"
-                  onChange={handleInputChange}
-                />
-                <input
-                  type="text"
-                  placeholder="About"
-                  name="bio"
-                  className="input about"
-                  onChange={handleInputChange}
-                />
-              </>
-            ) : null} */}
+            {errorMsg !== "" ? (
+              <div className="autherror">{errorMsg}</div>
+            ) : null}
             <input
               type="text"
               placeholder="email"
               className="input"
               name="email"
               onChange={handleInputChange}
+              ref={emailRef}
             />
             <input
               type="password"
@@ -118,6 +123,7 @@ export default function Authenticate(props) {
               className="input"
               name="password"
               onChange={handleInputChange}
+              ref={passwordRef}
             />
             <input
               type="submit"
